fe3569017fe5e2757612fbcd47d470269c218ad3
sojitradharti <46639075+sojitradharti@users.noreply.github.com>
2020-09-29 13:58:01 -0400

    changes in routes path server.js (#16)
    * typo in pull request workflows
    
    * Changes in working directory yml file
    
    * changes in comment
    
    * routes path server.js
    
    * added mock functions for testing purpose

--- webappBackend/test/user.test.js
+++ webappBackend/test/user.test.js
@@ -1,44 +1,18 @@
+var assert = require('assert');
+var expect = require('chai').expect;
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-const should = require('should');
-
-chai.use(chaiHttp);
-
-describe('/GET /api/accounts', () => {
-    it('it should GET all the users', (done) => {
-      chai.request("http://localhost:3000/")
-          .get('api/accounts')
-          .end((err, res) => {
-               console.log(res.body);
-            
-            done();
-          });
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.strictEqual([1, 2, 3].indexOf(4), -1);
     });
+  });
 });
-// const request = require('supertest');
-// const express = require('express');
-// var bodyParser = require('body-parser');
 
-// const app = express();
-// app.use(bodyParser.json())
-// require('../api/routes/routes')(app);
-
-// describe('Post /api/accounts', function(req,res) {
-//     it('responds with json', function(done) {
-//       request(app)
-//         .post('/api/accounts')
-//         .send({
-//             "firstname": "",
-//             "lastname": "1",
-//             "password": "Parker@649",
-//             "email": "1@a.com"})
-//         .set('Accept', 'application/json')
-//         .expect('Content-Type', /json/)
-//         .expect(400)
-//         .end(function(err, res) {
-//             if (err) return done(err);
-//             done();
-//           });
-//     });
-//   });
+describe('length of array', function () {
+  describe('#length()', function () {
+    it('should return length of array', function () {
+     expect([1, 2, 3]).to.have.length.of.at.least(3);
+    });
+  });
+});
