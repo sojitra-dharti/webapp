6277fc0acac464dbf736853b37c9b62e4316ff1c
sojitradharti <46639075+sojitradharti@users.noreply.github.com>
2020-10-01 00:09:23 -0400

    changes in formatting (#27)

--- webappBackend/server.js
+++ webappBackend/server.js
@@ -5,10 +5,6 @@ const cors = require("cors");
 const port =3000;
 const app = express();
 
-//auth function
-
-
-
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
@@ -23,8 +19,6 @@ var routes = require('./api/routes/routes');
 
 //register the route
 routes(app); 
-
-
 
 //to sync database
 models.sequelize.sync().then(function() {
