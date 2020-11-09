d32589fea7ae4c966bcfb0580a4bd4089dfeaee3
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-11-09 17:04:03 -0500

    changes in after install steps

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,16 @@
 
-
+pwd
+ls -lrt
 sudo unzip build_artifact.zip
+
+pwd
+ls -lrt
 cd ..
 
 sudo cp /home/ubuntu/webapp/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/
+x
+sleep 3
+
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
+	-a fetch-config -m ec2 \
+	-c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json -s
