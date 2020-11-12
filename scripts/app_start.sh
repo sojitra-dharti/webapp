94da595d87fca2e77be4a6a008711d6963e6eb15
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-11-11 20:01:23 -0500

    changed permissions of all paths

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo chmod 666 /home/ubuntu/webapp
+sudo chmod 666 /home/ubuntu/webapp/webappBackend
+sudo chmod 666 /home/ubuntu/webapp/webappBackend/logs/csye6225.log
 cd /home/ubuntu/webapp/webappBackend
-sudo chmod 666 /logs/csye6225.log
+
 sudo nohup node server.js >> debug.log 2>&1 &
