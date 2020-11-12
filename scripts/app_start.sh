356c6e6396601e04f6a61e8f4cae7a710c7cd25b
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-11-11 21:04:09 -0500

    Changed 777 permissions to 666

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,6 +2,13 @@
 
 cd /home/ubuntu/webapp/webappBackend
 
-sudo nohup node server.js >> debug.log 2>&1 &
-sudo chmod 666 /home/ubuntu/webapp/webappBackend/logs
-sudo chmod 666 /home/ubuntu/webapp/webappBackend/logs/csye6225.log
+if [ -d "logs" ] 
+then
+    echo "Directory /home/ubuntu/webapp/webappBackend/logs exists." 
+else
+    sudo mkdir -p logs
+    sudo touch logs/csye6225.log
+    sudo chmod 666 logs/csye6225.log
+fi
+
+sudo nohup node server.js >> debug.log 2>&1 
