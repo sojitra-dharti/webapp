fd2db7f29d34146f7d80c6ae5b656af1b995e8e2
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-11-11 20:03:47 -0500

    changed permissions of all paths

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,6 +2,7 @@
 
 sudo chmod 666 /home/ubuntu/webapp
 sudo chmod 666 /home/ubuntu/webapp/webappBackend
+sudo chmod 666 /home/ubuntu/webapp/webappBackend/logs
 sudo chmod 666 /home/ubuntu/webapp/webappBackend/logs/csye6225.log
 cd /home/ubuntu/webapp/webappBackend
 
