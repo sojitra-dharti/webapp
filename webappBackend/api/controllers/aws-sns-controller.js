ffafcf24c07f9de81bc039925838b31e465dab50
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-12-01 12:17:31 -0500

    changes in dynamo db logic

--- webappBackend/api/controllers/aws-sns-controller.js
+++ webappBackend/api/controllers/aws-sns-controller.js
@@ -3,40 +3,51 @@ const dynamoClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 var log4js = require('../../config/log4js');
 const logger = log4js.getLogger('logs');
+const timeController = require('../controllers/time-controller');
 
-exports.publishSNS = (params) => {
+exports.publishSNS = async (message) => {
+  let params = JSON.parse(message.Message);
+  console.log(params);
     var searchParams = {
         TableName: "csye6225",
         Key: {
-          id: params.email
+          id: params.Email+params.QuesId+params.AnsId
         }
       };
     
-      dynamoClient.get(searchParams, (err, data) => {
+       await dynamoClient.get(searchParams, (err, data) => {
         if (err) {
-            console.log(err);
-            logger.error("Ddynamo db access problem for " + params.QuesId);
+          console.log("Ddynamo db access problem");
+           logger.error("Ddynamo db access problem for " + params.QuesId);
            
         } else if (data.Item == null || data.Item == undefined) {
-            logger.info("5");
-            logger.info("Sns notification has already been sent for the question " + params.Question );
-            
+           
+            logger.info("Sns notification sent for the question " + params.Question );
+            console.log("Sns notification sent for the question");
+            dynamoClient.put(searchParams ,function (err,data){
+              if(err){
+              console.log(err);
+              }
+              else{
+              console.log(data);
+              console.log("dynamo  put error");
+              }
+            });      
+
             var publishTextPromise = sns.publish(params).promise();
             logger.info("1");
             publishTextPromise.then(
               function (data) {
                 logger.info("2");
-                  // logger.info(`Message ${params.Message} sent to the topic ${params.TopicArn}`);
-                  // logger.info(`Message ${params.Message.email}`);
               }).catch(
                   function (err) {
                     logger.info("3");
-                     // logger.error("Error in publishing SNS");
+                     console.log("Error in publishing SNS");
                   });
         }
     });
 
-   
+
 
 }
 
