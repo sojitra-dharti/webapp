3593a3b2c69bb70ec7002f7a06fccbed54d71b97
Dharti Sojitra <sojitra.d@husky.neu.edu>
2020-12-09 17:28:12 -0500

    added sql logs

--- webappBackend/api/models/index.js
+++ webappBackend/api/models/index.js
@@ -1,6 +1,10 @@
 const dbConfig = require("../../config/db.config.js");
 
 const Sequelize = require("sequelize");
+
+const { QueryTypes } = require('sequelize');
+var log4js = require('../../config/log4js');
+const logger = log4js.getLogger('logs');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
@@ -16,8 +20,10 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 
 const db = {};
 
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+const Op = db.Sequelize.Op;
 
 db.users = require("./User.js")(sequelize, Sequelize);
 db.question = require("./Question.js")(sequelize, Sequelize);
@@ -107,5 +113,17 @@ db.file.belongsTo(db.answer, {
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
+db.sequelize.query("SHOW STATUS LIKE 'Ssl_%'", {
+  type: QueryTypes.SELECT
+}).then((result) => {
+  // console.log(result[0].Value);
+  if(result == undefined || result == null || result.length == 0){
+      logger.info(`RDS DB SSL Cipher check info: SSL data not available`, {tags: 'http', additionalInfo: {result: JSON.parse(JSON.stringify(result))}});
+  } else {     
+      logger.info(`RDS DB SSL Cipher check info query: SHOW STATUS LIKE 'Ssl_%'; Result: `, {tags: 'http', additionalInfo: {result: JSON.parse(JSON.stringify(result))}});
+     }
+}).catch(err => {
+  logger.error(`Error in RDS DB SSL Cipher check: `, {tags: 'http', additionalInfo: {error: err}});
+});
 
 module.exports = db;
